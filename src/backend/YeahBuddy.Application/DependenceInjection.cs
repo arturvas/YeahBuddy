96cb00a1947f2f2b26b97a54dc258c95b2985a2d
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-10-10 13:55:57 -0400

    initial template of `DependenceInjection` for `YeahBuddy.Application`

---
+++ src/backend/YeahBuddy.Application/DependenceInjection.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YeahBuddy.Application;
+
+public static class DependenceInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+    }
+}
