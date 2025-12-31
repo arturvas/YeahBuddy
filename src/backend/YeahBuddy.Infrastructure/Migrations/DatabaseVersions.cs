069ecd94ebf2090a98e71f6c3b8309d2def687e1
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-12-31 18:34:58 -0300

    refactor: replace EF Core migrations with FluentMigrator
    - Remove Entity Framework Core migrations and related files.
    - Add FluentMigrator package for database migration.
    - Introduce `DatabaseVersions` abstraction to track migration versions.
    - Create initial migration with `Version0000001`.

---
+++ src/backend/YeahBuddy.Infrastructure/Migrations/DatabaseVersions.cs
@@ -0,0 +1,6 @@
+namespace YeahBuddy.Infrastructure.Migrations;
+
+public abstract class DatabaseVersions
+{
+    public const int TableUser = 1;
+}
---
+++ src/backend/YeahBuddy.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,12 @@
+using FluentMigrator;
+
+namespace YeahBuddy.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TableUser, "InitialCreate")]
+public class Version0000001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        throw new NotImplementedException();
+    }
+}
